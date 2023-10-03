ecf61d35e1a46b6fe83e1751d8c666495d698b46
Maximus1198 <scrappy11112222@hotmail.com>
2023-10-03 13:20:44 +0000

    [MIGRATE] indent_blankline v3

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,18 +1,18 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
-  return
+	return
 end
 
 vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 vim.g.indent_blankline_filetype_exclude = {
-  "help",
-  "alpha",
-  "dashboard",
-  "packer",
-  "neogitstatus",
-  "NvimTree",
-  "Trouble",
-  "text"
+	"help",
+	"alpha",
+	"dashboard",
+	"packer",
+	"neogitstatus",
+	"NvimTree",
+	"Trouble",
+	"text",
 }
 vim.g.indentLine_enabled = 1
 vim.g.indent_blankline_char = "│"
@@ -25,26 +25,26 @@ vim.g.indent_blankline_use_treesitter = false
 -- vim.g.indent_blankline_use_treesitter_scope = 1
 vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_context_patterns = {
-  "class",
-  "return",
-  "function",
-  "method",
-  "^if",
-  "^while",
-  "jsx_element",
-  "^for",
-  "^object",
-  "^table",
-  "block",
-  "arguments",
-  "if_statement",
-  "else_clause",
-  "jsx_element",
-  "jsx_self_closing_element",
-  "try_statement",
-  "catch_clause",
-  "import_statement",
-  "operation_type",
+	"class",
+	"return",
+	"function",
+	"method",
+	"^if",
+	"^while",
+	"jsx_element",
+	"^for",
+	"^object",
+	"^table",
+	"block",
+	"arguments",
+	"if_statement",
+	"else_clause",
+	"jsx_element",
+	"jsx_self_closing_element",
+	"try_statement",
+	"catch_clause",
+	"import_statement",
+	"operation_type",
 }
 -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
 -- vim.wo.colorcolumn = "99999"
@@ -60,19 +60,48 @@ vim.g.indent_blankline_context_patterns = {
 -- vim.opt.listchars:append "space:"
 -- vim.opt.listchars:append "eol:↴"
 
-indent_blankline.setup {
-  show_end_of_line = true,
-  space_char_blankline = " ",
-  -- char = "",
-  -- context_char = "│",
-  strict_tabs = true,
-  show_current_context = true,
-  -- show_current_context_start = true,
-  max_indent_increase = 1,
-  show_trailing_blankline_indent = false,
-  char_highlight_list = {
-    -- "IndentBlanklineIndent1",
-    -- "IndentBlanklineIndent2",
-    -- "IndentBlanklineIndent5",
-  },
-}
+indent_blankline.setup({
+	enabled = true,
+	exclude = {
+		filetypes = { "" },
+		buftypes = {
+			"terminal",
+			"nvim-tree",
+			"help",
+			"alpha",
+			"dashboard",
+			"packer",
+			"neogitstatus",
+			"NvimTree",
+			"Trouble",
+			"text",
+		},
+	},
+	debounce = 250,
+	viewport_buffer = {
+		min = 100,
+		max = 600,
+	},
+	indent = {
+		-- char = "|",
+		-- tab_char = { "a", "b", "c" },
+		-- highlight = { "Function", "Search" },
+		smart_indent_cap = true,
+		priority = 2,
+	},
+	scope = {
+		enabled = true,
+		show_start = true,
+		show_end = false,
+		injected_languages = false,
+		highlight = { "Function", "Label" },
+		priority = 500,
+	},
+	whitespace = {
+		highlight = { "Function", "Label" },
+		remove_blankline_trail = true,
+	},
+	-- space_char_blankline = " ",
+	-- char = "",
+	-- context_char = "│",
+})
